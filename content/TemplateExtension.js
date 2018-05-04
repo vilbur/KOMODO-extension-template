92e8a4448ec3f0756a877388e0defb247dd7219e
Unknown <cerny.vil@gmail.com>
2018-05-04 17:20:23 +0200

    + TemplateExtension

---
+++ content/TemplateExtension.js
@@ -0,0 +1,41 @@
+
+/** TemplateExtension
+ */
+ko.extensions.TemplateExtension = {};
+(function()
+{
+
+	function TemplateExtension()
+	{
+		/*--------   PRIVATE PROPERTIES   -------------------------------------------------------------------------------------*/
+		var _this	= this;
+		var Logger	= ko.extensions.Logger_v3 ? new ko.extensions.Logger_v3(this).clear(true).off(false) : require('ko/console');
+				
+		/*--------   PUBLIC METHODS   --------------------------------------------------------------------------------------*/
+		/** menuTest
+		 */
+		this.menuTest = function(string)
+		{
+			var bundleSvc = Components.classes["@mozilla.org/intl/stringbundle;1"].getService(Components.interfaces.nsIStringBundleService);
+			var _locale = bundleSvc.createBundle("chrome://TemplateExtension/locale/TemplateExtension.properties");
+			/** GetLocalizedString
+			 */
+			var GetLocalizedString = function(str) {
+				return _locale.GetStringFromName(str);
+			};
+			
+			alert('ko.extensions.TemplateExtension.Test("'+string+'")\n\n'+GetLocalizedString('property.property_test') );
+			
+		};
+		/** 
+		 */
+		this.callbackTest = function(value='')
+		{
+			console.log( value );			
+		};
+		
+	}
+
+	return TemplateExtension;
+
+})().apply(ko.extensions.TemplateExtension);
